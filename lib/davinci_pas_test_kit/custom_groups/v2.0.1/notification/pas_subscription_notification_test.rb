fd4009f2b762ae19a91ed733873044d9179daaa6
Karl Naden <knaden@mitre.org>
2024-08-07 12:03:43 -0400

    consistent naming

--- lib/davinci_pas_test_kit/custom_groups/v2.0.1/notification/pas_subscription_notification_test.rb
+++ lib/davinci_pas_test_kit/custom_groups/v2.0.1/notification/pas_subscription_notification_test.rb
@@ -15,17 +15,17 @@ module DaVinciPASTestKit
       )
 
       run do
-        identifier = SecureRandom.hex(32)
+        token = SecureRandom.hex(32)
         # rubocop:disable Layout/LineLength
         wait(
-          identifier:,
+          identifier: token,
           message: %(
             Inferno has received a 'Pended' claim response indicating that a final decision
             has not yet been made on the prior authorization request.
 
             Please
             **[click
-            here](#{Inferno::Application['base_url']}/custom/davinci_pas_server_suite_v201/resume_after_notification?token=#{identifier})**
+            here](#{Inferno::Application['base_url']}/custom/davinci_pas_server_suite_v201/resume_after_notification?token=#{token})**
             when the status of this claim has been finalized to inform Inferno to resume testing.
 
             Future versions of this test will validate subscription-based notifications as
