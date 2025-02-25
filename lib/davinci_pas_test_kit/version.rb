0fb5ae79f97c9a6d363b74247582a2389a42ed8d
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-02-25 09:52:57 -0500

    Bump version to 0.12.0

--- lib/davinci_pas_test_kit/version.rb
+++ lib/davinci_pas_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module DaVinciPASTestKit
-  VERSION = '0.11.1'
-  LAST_UPDATED = '2025-02-04' # TODO: update next release
+  VERSION = '0.12.0'
+  LAST_UPDATED = '2025-02-25'
 end
