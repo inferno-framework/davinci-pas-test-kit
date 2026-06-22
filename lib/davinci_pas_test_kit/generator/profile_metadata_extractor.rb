d07070f05e0672f13a841eedae9f82958e3c5b7c
Lucas Tucker <ltucker@leaporbit.com>
2026-06-22 17:03:34 -0400

    Migrate Metadata Updates out of PAS

--- lib/davinci_pas_test_kit/generator/profile_metadata_extractor.rb
+++ lib/davinci_pas_test_kit/generator/profile_metadata_extractor.rb
@@ -1,7 +1,7 @@
 require_relative 'profile_metadata'
 require_relative 'ig_metadata'
 require_relative 'terminology_binding_metadata_extractor'
-require_relative 'pas_must_support_metadata_extractor'
+require 'inferno/dsl/must_support_metadata_extractor'
 
 module DaVinciPASTestKit
   class Generator
@@ -148,7 +148,7 @@ module DaVinciPASTestKit
 
       def must_support_metadata_extractor
         @must_support_metadata_extractor ||=
-          PASMustSupportMetadataExtractor.new(profile_elements, profile, resource, ig_resources)
+          Inferno::DSL::MustSupportMetadataExtractor.new(profile_elements, profile, resource, ig_resources)
       end
 
       def must_supports
