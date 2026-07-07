0926058f2ad3a9e6a03ec8ef67061ed65aa16f34
Lucas Tucker <128156413+ljtucker@users.noreply.github.com>
2026-07-07 13:48:42 -0400

    Update lib/davinci_pas_test_kit/client/v2.2.1/pas_client_error_handling_group.rb
    Co-authored-by: Karl Naden <86612928+karlnaden@users.noreply.github.com>

--- lib/davinci_pas_test_kit/client/v2.2.1/pas_client_error_handling_group.rb
+++ lib/davinci_pas_test_kit/client/v2.2.1/pas_client_error_handling_group.rb
@@ -7,12 +7,9 @@ module DaVinciPASTestKit
       id :pas_client_v221_error_handling_group
       title 'Error Handling'
       description %(
-        The error handling tests verify that the client can appropriately handle prior
-        authorization error responses from the server, including both HTTP-level operation
+        The error handling tests verify that the client can appropriately handle [prior
+        authorization error responses](https://hl7.org/fhir/us/davinci-pas/2.2.1/en/specification.html#prior-authorization-transaction-error-handling) from the server, including both HTTP-level operation
         failures and business-level processing errors conveyed within a response bundle.
-
-        These tests correspond to the requirements in IG §7.2.5 (Prior Authorization
-        Transaction Error Handling).
       )
 
       group from: :pas_client_v221_operation_failure_group
