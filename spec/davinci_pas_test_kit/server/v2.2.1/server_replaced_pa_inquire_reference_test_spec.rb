a08aef40fafceb346eebf13244e5a585b7207f01
Tony Dao <tonydao1@bu.edu>
2026-08-18 08:57:33 -0400

    Fixed merge conflict

---
+++ spec/davinci_pas_test_kit/server/v2.2.1/server_replaced_pa_inquire_reference_test_spec.rb
@@ -0,0 +1,127 @@
+RSpec.describe DaVinciPASTestKit::DaVinciPASV221::PASServerReplacedPAInquireReferenceTest, :runnable do
+  let(:suite_id) { 'davinci_pas_server_suite_v221' }
+  let(:server_endpoint) { 'http://example.com/fhir' }
+
+  let(:test) do
+    Class.new(described_class) do
+      fhir_client { url :server_endpoint }
+      input :server_endpoint
+    end
+  end
+
+  def request_bundle(reference_number)
+    {
+      resourceType: 'Bundle',
+      type: 'collection',
+      entry: [
+        {
+          resource: {
+            resourceType: 'Claim',
+            item: [
+              {
+                sequence: 1,
+                extension: [
+                  {
+                    url: described_class::AUTHORIZATION_NUMBER_EXTENSION,
+                    valueString: reference_number
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    }
+  end
+
+  def response_parameters(reference_number)
+    {
+      resourceType: 'Parameters',
+      parameter: [
+        {
+          name: 'return',
+          resource: {
+            resourceType: 'Bundle',
+            type: 'collection',
+            entry: [
+              {
+                resource: {
+                  resourceType: 'ClaimResponse',
+                  item: [
+                    {
+                      itemSequence: 1,
+                      extension: [
+                        {
+                          url: described_class::AUTHORIZATION_NUMBER_EXTENSION,
+                          valueString: reference_number
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }.to_json
+  end
+
+  def stub_inquire(response_body)
+    stub_request(:post, "#{server_endpoint}/Claim/$inquire")
+      .to_return(status: 200, body: response_body)
+  end
+
+  it 'fails when provided request body does not contain a reference number' do
+    bundle = request_bundle('')
+
+    result = run(
+      test,
+      server_endpoint:,
+      replaced_pa_inquire_request_body: bundle.to_json
+    )
+
+    expect(result.result).to eq('fail')
+    expect(result.result_message).to include('inquiry request did not contain an authorization number')
+  end
+
+  it 'passes with a JSON Bundle when the response contains a different reference number' do
+    bundle = request_bundle('AUTH-0001')
+    stub_inquire(response_parameters('AUTH-0002'))
+
+    result = run(
+      test,
+      server_endpoint:,
+      replaced_pa_inquire_request_body: bundle.to_json
+    )
+
+    expect(result.result).to eq('pass')
+  end
+
+  it 'passes with an array wrapped Bundle when the response contains a different reference number' do
+    bundle = request_bundle('AUTH-0001')
+    stub_inquire(response_parameters('AUTH-0002'))
+
+    result = run(
+      test,
+      server_endpoint:,
+      replaced_pa_inquire_request_body: [bundle].to_json
+    )
+
+    expect(result.result).to eq('pass')
+  end
+
+  it 'fails when the response contains the same reference number' do
+    bundle = request_bundle('AUTH-0001')
+    stub_inquire(response_parameters('AUTH-0001'))
+
+    result = run(
+      test,
+      server_endpoint:,
+      replaced_pa_inquire_request_body: bundle.to_json
+    )
+
+    expect(result.result).to eq('fail')
+    expect(result.result_message).to include('different authorization number')
+  end
+end
